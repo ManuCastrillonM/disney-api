7855ad1aac221ca9265df0e6eb426940856b2a40
Manu Castrillon <manu.cm43@gmail.com>
2020-07-03 15:53:46 -0500

    remove unused var

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
 class Config {
   constructor() {
-    this.MODE = process.env.MODE || 'dev'
     this.PORT = process.env.PORT || 8080
     this.MONGO_URL =
       process.env.MONGODB_URI || 'mongodb://localhost:27017/disney-api'
