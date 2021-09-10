b4890562814b5087b0e48232dd6ede806654c08b
ManuCastrillonM <manu.cm43@gmail.com>
2021-09-09 22:44:59 -0500

    rename app.js
    include graphql base implementation.
    
    Co-authored-by: Sebastian Vargas L <svargaslondono@gmail.com>

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,55 @@
+const { buildSchema } = require('graphql');
 const { json } = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const cors = require('cors');
 
 const config = require('./config');
 const router = require('../routes');
+
+const Character = require('../models/Character');
+
+var schema = buildSchema(`
+  type Query {
+    character(_id: Int!): Character
+    characters: [Character]
+  },
+  type Character {
+    _id: Int
+    url: String
+    name: String
+    sourceUrl: String
+    imageUrl: String
+    films: [String]
+    shortFilms: [String]
+    tvShows: [String]
+    videoGames: [String]
+    alignment: String
+    parkAttractions: [String]
+    allies: [String]
+    enemies: [String]
+  }
+`);
+
+// Return a single user (based on id)
+const getCharacter = async function (args) {
+  const item = await Character.findById(args._id).select().lean().exec();
+  return item;
+};
+
+// Return a list of users (takes an optional shark parameter)
+const getCharacters = async function (args) {
+  const items = await Character.find().select().lean().exec();
+  return items;
+};
+
+// Root resolver
+var root = {
+  character: getCharacter, // Resolver function to return user with specific id
+  characters: getCharacters // Resolver function to return all users
+};
 
 module.exports = (app) => {
   mongoose.connect(config.MONGO_URL, {
@@ -27,4 +72,12 @@ module.exports = (app) => {
   app.use(cors());
   app.use(json());
   app.use('/', router);
+  app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema: schema, // Must be provided
+      rootValue: root,
+      graphiql: true // Enable GraphiQL when server endpoint is accessed in browser
+    })
+  );
 };
