b08ab31e150235476b7647cd23a94783462a5174
manuCastrillonM <manu.cm43@gmail.com>
2023-04-10 19:03:29 +0200

    Refactor /character endpoint
    in order to deprecate the /characters endpoint, the /character endpoint now covers all the /characters use cases like return a list of characters and filter characters by id

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,18 @@ const router = require('express').Router();
 const characterService = require('../services/character');
 
 const endpoints = {
-  getAllCharacters: '/characters',
-  filterCharacter: '/character',
-  getOneCharacterById: '/characters/:id'
+  getAllCharacters: '/characters', // TODO: Deprecate
+  getOneCharacterById: '/characters/:id', // TODO: Deprecate
+  getCharacter: '/character',
+  getCharacterById: '/character/:id'
 };
 
 router.get('/', (req, res) => {
   res.json(endpoints);
 });
 
-router.get(endpoints.filterCharacter, characterService.filterCharacter);
+router.get(endpoints.getCharacter, characterService.getCharacter);
+router.get(endpoints.getCharacterById, characterService.getCharacter);
 router.get(endpoints.getAllCharacters, characterService.getAll);
 router.get(endpoints.getOneCharacterById, characterService.getOneById);
 
--- src/services/crud.js
+++ src/services/crud.js
@@ -1,21 +1,14 @@
-const url = require('url');
 const { searchParamsToRegexQuery } = require('../utils');
 
 const DEFAULT_PAGE_SIZE = 50;
 
 const getAll = (model) => async (req, res) => {
   try {
+    const reqUrl = req.protocol + '://' + req.get('host') + req.route.path;
     const pageSize = parseInt(req.query.pageSize) || DEFAULT_PAGE_SIZE;
     const page = parseInt(req.query.page) || 1;
-    const skip = (page - 1) * pageSize;
 
-    const items = await model
-      .find()
-      .select(
-        '_id name imageUrl url films shortFilms tvShows videoGames parkAttractions allies enemies'
-      )
-      .skip(skip)
-      .limit(pageSize);
+    const items = await getAllCharacters(model, pageSize, page);
 
     if (!items) {
       res.status(400).end();
@@ -27,19 +20,17 @@ const getAll = (model) => async (req, res) => {
     let itemsRes = {
       data: items,
       count: items.length,
-      totalPages: totalPages
+      totalPages: totalPages,
+      warning:
+        '🚨 IMPORTANT: THIS ENDPOINT IS GOING TO BE DEPRECATED ON APRIL 30TH. PLEASE USE /character INSTEAD 🚨'
     };
 
     if (page > 1) {
-      itemsRes.previousPage = `https://api.disneyapi.dev${
-        req.route.path
-      }?page=${page - 1}`;
+      itemsRes.previousPage = `${reqUrl}?page=${page - 1}`;
     }
 
     if (page < totalPages) {
-      itemsRes.nextPage = `https://api.disneyapi.dev${req.route.path}?page=${
-        page + 1
-      }`;
+      itemsRes.nextPage = `${reqUrl}?page=${page + 1}`;
     }
 
     res.status(200).json(itemsRes);
@@ -51,41 +42,16 @@ const getAll = (model) => async (req, res) => {
 
 const getOneById = (model) => async (req, res) => {
   try {
-    const item = await model
-      .findById(req.params.id)
-      .select(
-        '_id name imageUrl url films shortFilms tvShows videoGames parkAttractions allies enemies'
-      )
-      .lean()
-      .exec();
+    const item = await getCharacterById(model, req.params.id);
 
+    const itemRes = {
+      item,
+      warning:
+        '🚨 IMPORTANT: THIS ENDPOINT IS GOING TO BE DEPRECATED ON APRIL 30TH. PLEASE USE /character INSTEAD 🚨'
+    };
     if (!item) {
       res.status(400).end();
     }
-    res.status(200).json(item);
-  } catch (e) {
-    console.log(e);
-    res.status(400).end();
-  }
-};
-
-const filterCharacter = (model) => async (req, res) => {
-  const searchParamsRegexQuery = searchParamsToRegexQuery(req.query);
-
-  try {
-    const item = await model
-      .find(searchParamsRegexQuery)
-      .select(
-        '_id name imageUrl url films shortFilms tvShows videoGames parkAttractions allies enemies'
-      )
-      .lean()
-      .exec();
-
-    const itemRes = {
-      count: item.length,
-      data: item
-    };
-
     res.status(200).json(itemRes);
   } catch (e) {
     console.log(e);
@@ -93,10 +59,94 @@ const filterCharacter = (model) => async (req, res) => {
   }
 };
 
+async function getAllCharacters(model, pageSize, page) {
+  const skip = (page - 1) * pageSize;
+
+  const characters = await model
+    .find()
+    .select(
+      '_id name imageUrl url films shortFilms tvShows videoGames parkAttractions allies enemies'
+    )
+    .skip(skip)
+    .limit(pageSize);
+
+  return characters;
+}
+
+async function getCharacterById(model, id) {
+  const characters = await model
+    .findById(id)
+    .select(
+      '_id name imageUrl url films shortFilms tvShows videoGames parkAttractions allies enemies'
+    )
+    .lean()
+    .exec();
+
+  return characters;
+}
+
+async function getCharacterByParams(model, searchParams) {
+  const searchParamsRegexQuery = searchParamsToRegexQuery(searchParams);
+
+  const characters = await model
+    .find(searchParamsRegexQuery)
+    .select(
+      '_id name imageUrl url films shortFilms tvShows videoGames parkAttractions allies enemies'
+    )
+    .lean()
+    .exec();
+
+  return characters;
+}
+
+const getCharacter = (model) => async (req, res) => {
+  const { page = 1, pageSize = DEFAULT_PAGE_SIZE, ...searchParams } = req.query;
+  const pageInt = parseInt(page);
+  const pageSizeInt = parseInt(pageSize);
+  const searchId = req.params.id;
+  const isFilterRequest = !searchId && Object.keys(searchParams).length > 0;
+
+  let items,
+    info = {};
+  const resJson = { info };
+
+  // Get character based on the given params
+  if (searchId) {
+    items = await getCharacterById(model, searchId);
+  } else if (isFilterRequest) {
+    items = await getCharacterByParams(model, searchParams);
+  } else {
+    items = await getAllCharacters(model, pageSizeInt, pageInt);
+
+    const reqUrl = `${req.protocol}://${req.get('host')}${req.route.path}`;
+    const totalDocuments = await model.estimatedDocumentCount();
+    const totalPages = Math.ceil(totalDocuments / pageSizeInt);
+
+    info.totalPages = totalPages;
+    info.previousPage =
+      page > 1 ? `${reqUrl}?page=${pageInt - 1}&pageSize=${pageSize}` : null;
+    info.nextPage =
+      page < totalPages
+        ? `${reqUrl}?page=${pageInt + 1}&pageSize=${pageSize}`
+        : null;
+  }
+
+  // If no items found, return 400
+  if (!items || items.length === 0) {
+    res.status(400).json({ error: 'No items found' });
+    return;
+  }
+
+  // If items found, return 200
+  resJson.data = items;
+  info.count = items.length || 1;
+  res.status(200).json(resJson);
+};
+
 const crudController = (model) => ({
-  filterCharacter: filterCharacter(model),
   getAll: getAll(model),
-  getOneById: getOneById(model)
+  getOneById: getOneById(model),
+  getCharacter: getCharacter(model)
 });
 
 module.exports = crudController;
